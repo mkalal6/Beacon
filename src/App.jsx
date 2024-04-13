a93ad839f8f2cd91b01c2baa8936a81a5b1d7c04
Kunal Singh <kunalsingh909090@gmail.com>
2024-04-13 13:43:15 -0500

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import  YourApp  from "./components/Login.jsx";
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Profile from './components/Profile.jsx'
 
 function App() {
 
   return (
-    <>
-    <YourApp></YourApp>
-    </>
+    
+     <BrowserRouter>
+    <Routes>
+    <Route path='/login' element = {<YourApp />}></Route>
+    <Route path='/user' element = {<Profile />}></Route>
+    </Routes>
+    </BrowserRouter>
+    
   )
 }
 
